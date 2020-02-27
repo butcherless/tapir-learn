45a8c550a924bf74cb113bb59d573fa8f8fee9f4
butcherless <cmarting@gmail.com>
2020-02-27 18:27:25 +0100

    chore(sbt): upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
   lazy val akkaHttpVersion = "10.1.11"
   lazy val akkaVersion = "2.6.3"
-  lazy val sttpVersion = "2.0.0"
-  lazy val tapirVersion = "0.12.22"
+  lazy val sttpVersion = "2.0.1"
+  lazy val tapirVersion = "0.12.23"
 
 
   lazy val logbackVersion = "1.2.3"
