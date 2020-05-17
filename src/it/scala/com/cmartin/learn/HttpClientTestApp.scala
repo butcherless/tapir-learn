d5c5dac73064d25ec2a182c63f21f3c9967d5675
Carlos Martin <cmarting@gmail.com>
2020-05-17 18:14:10 +0200

    fix: zio.random signature change

--- src/it/scala/com/cmartin/learn/HttpClientTestApp.scala
+++ src/it/scala/com/cmartin/learn/HttpClientTestApp.scala
@@ -12,8 +12,8 @@ import zio.{App, Schedule, Task, UIO, ZIO}
 object HttpClientTestApp extends App {
 
   val healthEndpoint = "http://localhost:8080/api/v1.0/health"
-  val fiberCount = 20
-  val loopCount = 500
+  val fiberCount     = 20
+  val loopCount      = 500
 
   // http client backend
   implicit val backend: SttpBackend[Task, Nothing, Nothing] =
@@ -21,29 +21,31 @@ object HttpClientTestApp extends App {
   val urls = List.fill(fiberCount)(healthEndpoint)
 
   // Dummy method
-  def makeGet(uri: String) = for {
-    number <- nextInt(1500)
-    delay <- UIO(number + 500)
-    _ <- ZIO.sleep(delay.milliseconds)
-    _ <- UIO(println(s"sleep[$delay]: $uri"))
-  } yield ()
+  def makeGet(uri: String) =
+    for {
+      number <- nextIntBetween(500, 2000)
+      delay  <- UIO(number)
+      _      <- ZIO.sleep(delay.milliseconds)
+      _      <- UIO(println(s"sleep[$delay]: $uri"))
+    } yield ()
 
   val program: ZIO[Clock with zio.ZEnv, Throwable, Unit] = for {
-    number <- nextInt(1500)
-    delay <- UIO(number + 500)
-    _ <- ZIO.sleep(delay.milliseconds)
-    _ <- ZIO.foreachParN(fiberCount)(urls)(doGet) repeat Schedule.recurs(loopCount - 1)
+    number <- nextIntBetween(500, 2000)
+    delay  <- UIO(number)
+    _      <- ZIO.sleep(delay.milliseconds)
+    _      <- ZIO.foreachParN(fiberCount)(urls)(doGet) repeat Schedule.recurs(loopCount - 1)
   } yield ()
 
-  def checkResponse(response: Response[Either[String, String]]) = response.code match {
-    case StatusCode.Ok => "Response Ok"
-    case _ => "Response Error"
-  }
+  def checkResponse(response: Response[Either[String, String]]) =
+    response.code match {
+      case StatusCode.Ok => "Response Ok"
+      case _             => "Response Error"
+    }
 
   def doGet(endpoint: String) =
     for {
       response <- basicRequest.get(uri"$endpoint").send()
-      _ <- putStrLn( checkResponse(response))
+      _        <- putStrLn(checkResponse(response))
     } yield ()
 
   // main function, needs exit = 0 [OK] or exit > 0 [ERROR]
