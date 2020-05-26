11ff4f586308434392dfae99fa43e7202fc6168a
Carlos Martin <cmarting@gmail.com>
2020-05-26 16:31:38 +0200

    chore: upgrade swagger.io version

--- src/main/scala/com/cmartin/learn/api/CustomSwaggerAkka.scala
+++ src/main/scala/com/cmartin/learn/api/CustomSwaggerAkka.scala
@@ -38,5 +38,5 @@ class CustomSwaggerAkka(yaml: String, contextPath: String = "docs", yamlName: St
 }
 
 object CustomSwaggerAkka {
-  private val swaggerVersion = "3.25.3"
+  private val swaggerVersion = "3.25.4"
 }
