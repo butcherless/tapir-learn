36bafb08ccfd2d616e2d07c973477df77e23df13
Carlos Martin <cmarting@gmail.com>
2020-08-13 12:07:26 +0200

    refactor: GET transfer unit tests

--- src/it/scala/com/cmartin/learn/SttpITSpec.scala
+++ src/it/scala/com/cmartin/learn/SttpITSpec.scala
@@ -2,7 +2,6 @@ package com.cmartin.learn
 
 import com.cmartin.learn.api.ApiModel.{BuildInfoDto, TransferDto}
 import com.cmartin.learn.api.TransferEndpoint
-import com.cmartin.learn.domain.ApiConverters
 import io.circe
 import io.circe.generic.auto._
 import org.scalatest.flatspec.AnyFlatSpec
@@ -10,7 +9,7 @@ import org.scalatest.matchers.should.Matchers
 import sttp.client.asynchttpclient.zio.{AsyncHttpClientZioBackend, SttpClient}
 import sttp.client.circe._
 import sttp.client.{basicRequest, _}
-import sttp.model.{Method, StatusCode}
+import sttp.model.StatusCode
 import zio.{UIO, ZIO}
 
 class SttpITSpec extends AnyFlatSpec with Matchers {
@@ -18,96 +17,6 @@ class SttpITSpec extends AnyFlatSpec with Matchers {
   val runtime = zio.Runtime.default
 
   behavior of "REST API"
-
-  //TODO move to unit tests
-  it should "respond Ok status stub backend for health request" in {
-    val dtoResponse: BuildInfoDto = ApiConverters.modelToApi()
-
-    val backend = AsyncHttpClientZioBackend.stub
-      .whenRequestMatches { req =>
-        req.method == Method.GET && req.uri.path.last == "health"
-      }
-      .thenRespond(dtoResponse)
-
-    val request =
-      basicRequest
-        .get(uri"http://localhost:8080/api/v1.0/health")
-        .response(asJson[BuildInfoDto])
-
-    val response: Response[Either[ResponseError[circe.Error], BuildInfoDto]] =
-      runtime
-        .unsafeRun(
-          backend.send(request)
-        )
-
-    response.code shouldBe StatusCode.Ok
-    response.body shouldBe dtoResponse
-  }
-
-  val backend = AsyncHttpClientZioBackend.stub
-    .whenRequestMatches { req =>
-      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "1"
-    }
-    .thenRespond(TransferEndpoint.transferExample)
-    .whenRequestMatches { req =>
-      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "400"
-    }
-    .thenRespond(Response("BAD_REQUEST", StatusCode.BadRequest))
-    .whenRequestMatches { req =>
-      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "500"
-    }
-    .thenRespond(Response("SERVER_ERROR", StatusCode.InternalServerError))
-
-
-  it should "TODO respond Ok for an existent transfer identifier" in {
-    val request =
-      basicRequest
-        .get(uri"http://localhost:8080/api/v1.0/transfers/1")
-        .response(asJson[TransferDto])
-
-    val response = runtime.unsafeRun(backend.send(request))
-
-    response.code shouldBe StatusCode.Ok
-    response.body shouldBe TransferEndpoint.transferExample
-  }
-
-  it should "TODO respond Bad Request for an invalid request" in {
-    val request =
-      basicRequest
-        .get(uri"http://localhost:8080/api/v1.0/transfers/400")
-        .response(asJson[TransferDto])
-
-    val response = runtime.unsafeRun(backend.send(request))
-
-    response.code shouldBe StatusCode.BadRequest
-    response.body.isLeft shouldBe true
-  }
-
-  it should "TODO respond Not Found for a missing transfer" in {
-    val request =
-      basicRequest
-        .get(uri"http://localhost:8080/api/v1.0/transfers/404")
-        .response(asJson[TransferDto])
-
-    val response = runtime.unsafeRun(backend.send(request))
-
-    response.code shouldBe StatusCode.NotFound
-    response.body.isLeft shouldBe true
-  }
-
-  it should "TODO respond Server Error for a server failure" in {
-    val request =
-      basicRequest
-        .get(uri"http://localhost:8080/api/v1.0/transfers/500")
-        .response(asJson[TransferDto])
-
-    val response = runtime.unsafeRun(backend.send(request))
-
-    response.code shouldBe StatusCode.InternalServerError
-    response.body.isLeft shouldBe true
-  }
-
-  //TODO end
 
   it should "respond Ok status for a health GET request" in {
     val request =
--- src/test/scala/com/cmartin/learn/api/TransferApiClientSpec.scala
+++ src/test/scala/com/cmartin/learn/api/TransferApiClientSpec.scala
@@ -1,5 +1,106 @@
 package com.cmartin.learn.api
 
-class TransferApiClientSpec {
+import com.cmartin.learn.api.ApiModel.{BuildInfoDto, TransferDto}
+import com.cmartin.learn.domain.ApiConverters
+import io.circe
+import io.circe.generic.auto._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.client.asynchttpclient.zio.AsyncHttpClientZioBackend
+import sttp.client.circe.asJson
+import sttp.client.{Response, ResponseError, basicRequest, _}
+import sttp.model.{Method, StatusCode}
+
+class TransferApiClientSpec extends AnyFlatSpec with Matchers {
+
+  val runtime = zio.Runtime.default
+
+  val backend = AsyncHttpClientZioBackend.stub
+    .whenRequestMatches { req =>
+      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "1"
+    }
+    .thenRespond(TransferEndpoint.transferExample)
+    .whenRequestMatches { req =>
+      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "400"
+    }
+    .thenRespond(Response("BAD_REQUEST", StatusCode.BadRequest))
+    .whenRequestMatches { req =>
+      req.method == Method.GET && req.uri.path.contains("transfers") && req.uri.path.last == "500"
+    }
+    .thenRespond(Response("SERVER_ERROR", StatusCode.InternalServerError))
+
+  behavior of "REST API Client"
+
+  it should "respond Ok status stub backend for health request" in {
+    val dtoResponse: BuildInfoDto = ApiConverters.modelToApi()
+
+    val backend = AsyncHttpClientZioBackend.stub
+      .whenRequestMatches { req =>
+        req.method == Method.GET && req.uri.path.last == "health"
+      }
+      .thenRespond(dtoResponse)
+
+    val request =
+      basicRequest
+        .get(uri"http://localhost:8080/api/v1.0/health")
+        .response(asJson[BuildInfoDto])
+
+    val response: Response[Either[ResponseError[circe.Error], BuildInfoDto]] =
+      runtime
+        .unsafeRun(
+          backend.send(request)
+        )
+
+    response.code shouldBe StatusCode.Ok
+    response.body shouldBe dtoResponse
+  }
+
+  it should "respond Ok for an existent transfer identifier" in {
+    val request =
+      basicRequest
+        .get(uri"http://localhost:8080/api/v1.0/transfers/1")
+        .response(asJson[TransferDto])
+
+    val response = runtime.unsafeRun(backend.send(request))
+
+    response.code shouldBe StatusCode.Ok
+    response.body shouldBe TransferEndpoint.transferExample
+  }
+
+  it should "respond Bad Request for an invalid request" in {
+    val request =
+      basicRequest
+        .get(uri"http://localhost:8080/api/v1.0/transfers/400")
+        .response(asJson[TransferDto])
+
+    val response = runtime.unsafeRun(backend.send(request))
+
+    response.code shouldBe StatusCode.BadRequest
+    response.body.isLeft shouldBe true
+  }
+
+  it should "respond Not Found for a missing transfer" in {
+    val request =
+      basicRequest
+        .get(uri"http://localhost:8080/api/v1.0/transfers/404")
+        .response(asJson[TransferDto])
+
+    val response = runtime.unsafeRun(backend.send(request))
+
+    response.code shouldBe StatusCode.NotFound
+    response.body.isLeft shouldBe true
+  }
+
+  it should "respond Server Error for a server failure" in {
+    val request =
+      basicRequest
+        .get(uri"http://localhost:8080/api/v1.0/transfers/500")
+        .response(asJson[TransferDto])
+
+    val response = runtime.unsafeRun(backend.send(request))
+
+    response.code shouldBe StatusCode.InternalServerError
+    response.body.isLeft shouldBe true
+  }
 
 }
