55c8ea8d165042e6f1cc7b249122eceb6248a09a
Carlos Martin <cmarting@gmail.com>
2020-08-20 16:39:08 +0200

    chore: upgrade sttp lib

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val akkaHttp = "10.2.0"
   val akka     = "2.6.8"
   val circe    = "0.13.0"
-  val sttp     = "2.2.4"
+  val sttp     = "2.2.5"
   val tapir    = "0.16.15"
 
   val json4s  = "3.6.9"
