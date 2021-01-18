2dcd69d44ae92ffd1e9c2950862f35bc07e78c98
Carlos Martin <cmarting@gmail.com>
2021-01-19 00:58:48 +0100

    chore: upgrade libs - tapir - akka test kit

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
 
-  val akkaHttp = "10.2.2"
+  val akkaHttp = "10.2.3"
   val akka     = "2.6.11"
   val circe    = "0.13.0"
   val sttp     = "2.2.9"
-  val tapir    = "0.17.4"
+  val tapir    = "0.17.5"
 
   val json4s  = "3.6.10"
   val logback = "1.2.3"
