b416974176f1ce6c08754892d06ac2f7c185af71
Scala Steward <me@scala-steward.org>
2021-02-16 16:09:07 +0100

    Update tapir-akka-http-server, tapir-core, ... to 0.17.11

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val akka     = "2.6.12"
   val circe    = "0.13.0"
   val sttp     = "2.2.9"
-  val tapir    = "0.17.10"
+  val tapir    = "0.17.11"
 
   val json4s  = "3.6.10"
   val logback = "1.2.3"
