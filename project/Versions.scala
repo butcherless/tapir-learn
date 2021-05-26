ed8167af5876738215d8a46ab1d4c70c0c232e6d
Carlos Martin <cmarting@gmail.com>
2021-05-26 09:57:42 +0200

    chore: downgrade json4s

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val sttp     = "3.3.4"
   val tapir    = "0.18.0-M11"
 
-  val json4s  = "4.0.0"
+  val json4s  = "3.6.11"
   val logback = "1.2.3"
   val slf4zio = "1.0.0"
   val zio     = "1.0.8"
