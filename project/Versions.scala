0698f9e06c0c47dcbea9e5f0f649a304e11cb1c6
Carlos Martin <cmarting@gmail.com>
2021-06-04 12:41:31 +0200

    chore: upgrade tapir chore: upgrade json4s lib

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,13 +4,12 @@ object Versions {
   val akka     = "2.6.14"
   val circe    = "0.13.0"
   val sttp     = "3.3.4"
-  val tapir    = "0.18.0-M11"
+  val tapir    = "0.18.0-M15"
 
-  val json4s  = "3.6.11"
+  val json4s  = "4.0.0"
   val logback = "1.2.3"
   val slf4zio = "1.0.0"
   val zio     = "1.0.9"
 
   val scalatest = "3.2.9"
-
 }
