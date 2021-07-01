69492bc0459f3682fb20ea79c7910f27bdcbfbc5
Scala Steward <me@scala-steward.org>
2021-07-01 16:16:16 +0200

    Update client3:async-http-client-backend-zio, ... to 3.3.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val json4s   = "4.0.0"
   val logback  = "1.2.3"
   val slf4zio  = "1.0.0"
-  val sttp     = "3.3.7"
+  val sttp     = "3.3.9"
   val tapir    = "0.18.0-M18"
   val zio      = "1.0.9"
 
