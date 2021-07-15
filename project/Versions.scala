792e07e6d153f95ecb41dbd4fe608ce134e7c696
Carlos Martin <cmarting@gmail.com>
2021-07-15 22:51:23 +0200

    chore: upgrade tapir

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val logback  = "1.2.3"
   val slf4zio  = "1.0.0"
   val sttp     = "3.3.7"
-  val tapir    = "0.18.0"
+  val tapir    = "0.19.0-M1"
   val zio      = "1.0.9"
 
   val scalatest = "3.2.9"
