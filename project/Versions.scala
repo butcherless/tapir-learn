212da7fc587d27629cd60030f393b3849f2f001b
Carlos Martin <cmarting@gmail.com>
2021-07-27 11:21:03 +0200

    chore: upgrade akka-http, json4s

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
 
-  val akkaHttp = "10.2.4"
+  val akkaHttp = "10.2.5"
   val akka     = "2.6.15"
-  val json4s   = "4.0.0"
+  val json4s   = "4.0.3"
   val logback  = "1.2.5"
   val slf4zio  = "1.0.0"
   val sttp     = "3.3.7"
