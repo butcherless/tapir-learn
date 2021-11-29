801cc51d95741dca610a2c74f8d3d44833c6ed4f
Scala Steward <me@scala-steward.org>
2021-11-30 00:38:00 +0100

    Update h2 to 2.0.202

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   // main
   val akkaHttp = "10.2.7"
   val akka = "2.6.17"
-  val h2 = "1.4.200"
+  val h2 = "2.0.202"
   val json4s = "4.0.3"
   val logback = "1.2.7"
   val slf4zio = "1.0.0"
