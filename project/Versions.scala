1e1160d25d5ad834c3782c4726f74ff45cf0c6b5
Scala Steward <me@scala-steward.org>
2022-04-02 06:05:49 +0200

    Update zio, zio-test, zio-test-sbt to 2.0.0-RC4

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val slick = "3.3.3"
   val sttp = "3.3.16"
   val tapir = "0.20.0-M7"
-  val zio = "2.0.0-M5"
+  val zio = "2.0.0-RC4"
   val zioLogging = "0.5.14"
   val zioPrelude = "1.0.0-RC8"
 
