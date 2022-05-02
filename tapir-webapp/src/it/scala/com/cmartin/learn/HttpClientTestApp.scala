34f2fef93cf86da90623b3dc72d0a08ec5407000
Carlos Martin <cmarting@gmail.com>
2022-05-02 14:06:45 +0200

    * disable integration test compilation

--- tapir-webapp/src/it/scala/com/cmartin/learn/HttpClientTestApp.scala
+++ tapir-webapp/src/it/scala/com/cmartin/learn/HttpClientTestApp.scala
@@ -21,7 +21,7 @@ object HttpClientTestApp extends App {
   val urls: Seq[String] = List.fill(fiberCount)(healthEndpoint)
   val program = for {
     number <- nextIntBetween(500, 2000)
-    delay <- UIO(number)
+    delay <- UIO.succeed(number)
     _ <- ZIO.sleep(delay.milliseconds)
     _ <- ZIO.foreachPar(urls)(doGet).withParallelism(fiberCount)
       .repeat(Schedule.recurs(loopCount - 1))
@@ -31,9 +31,9 @@ object HttpClientTestApp extends App {
   def makeGet(uri: String) =
     for {
       number <- nextIntBetween(500, 2000)
-      delay <- UIO(number)
+      delay <- UIO.succeed(number)
       _ <- ZIO.sleep(delay.milliseconds)
-      _ <- UIO(println(s"sleep[$delay]: $uri"))
+      _ <- UIO.succeed(println(s"sleep[$delay]: $uri"))
     } yield ()
 
   def doGet(endpoint: String) =
