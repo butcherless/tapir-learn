403b232c212ba00410015c289f9c749d114ea91f
Carlos Martin <cmarting@gmail.com>
2022-06-04 07:20:37 +0200

    * upgrade tapir

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,6 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-slf4j" % Versions.akka,
     "ch.qos.logback" % "logback-classic" % Versions.logback,
     "com.github.mlangc" %% "slf4zio" % Versions.slf4zio,
-    "dev.zio" %% "zio" % Versions.zio,
     "dev.zio" %% "zio-prelude" % Versions.zioPrelude,
     "org.json4s" %% "json4s-ext" % Versions.json4s,
     "org.json4s" %% "json4s-native" % Versions.json4s,
@@ -66,8 +65,6 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % Versions.scalatest % "it, test",
     "com.typesafe.akka" %% "akka-http-testkit" % Versions.akkaHttp % Test,
     "com.typesafe.akka" %% "akka-testkit" % Versions.akka % Test,
-    "dev.zio" %% "zio-test" % Versions.zio % "test",
-    "dev.zio" %% "zio-test-sbt" % Versions.zio % "test",
     // S T T P
     "com.softwaremill.sttp.client3" %% "core" % Versions.sttp, // % "it, test",
     "com.softwaremill.sttp.client3" %% "async-http-client-backend-zio" % Versions.sttp, // % "it, test",
