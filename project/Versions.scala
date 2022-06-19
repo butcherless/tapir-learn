65a510f07eda46b5caa2e6f68ea71773bfb59283
Carlos Martin <cmarting@gmail.com>
2022-06-19 09:59:00 +0200

    * upgrade h2

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   // main
   val akkaHttp   = "10.2.9"
   val akka       = "2.6.19"
-  val h2         = "2.1.212"
+  val h2         = "2.1.214"
   val json4s     = "4.0.5"
   val logback    = "1.2.11"
   val slf4zio    = "1.0.0"
