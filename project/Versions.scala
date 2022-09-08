ef350c946089c8efaa329fa18652939bb605dfef
Carlos Martin <cmarting@gmail.com>
2022-09-08 13:52:10 +0200

    * upgrade tapir * upgrade akka test kit

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,14 +1,14 @@
 object Versions {
   // main
   val akkaHttp   = "10.2.9"
-  val akka       = "2.6.19"
+  val akka       = "2.6.20"
   val h2         = "2.1.214"
   val json4s     = "4.0.5"
   val logback    = "1.2.11"
   val slf4zio    = "1.0.0"
   val slick      = "3.4.0"
   val sttp       = "3.6.2"
-  val tapir      = "1.0.6"
+  val tapir      = "1.1.0"
   val zioLogging = "2.1.0"
   val zioPrelude = "1.0.0-RC15"
 
