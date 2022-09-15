dc871c49ef1bccd18bb458bf4a6bfa6a7e743a7e
Carlos Martin <cmarting@gmail.com>
2022-09-15 09:35:37 +0200

    * upgrade slick lib

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val json4s     = "4.0.5"
   val logback    = "1.2.11"
   val slf4zio    = "1.0.0"
-  val slick      = "3.4.0"
+  val slick      = "3.4.1"
   val sttp       = "3.6.2"
   val tapir      = "1.1.0"
   val zioLogging = "2.1.1"
