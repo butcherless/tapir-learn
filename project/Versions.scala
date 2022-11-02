ead7bd42fdd6611953983254c7de3fcf9e11feef
Scala Steward <scala_steward@virtuslab.com>
2022-11-02 12:39:56 +0000

    Revert commit(s) 6724d03

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val json4s     = "4.0.5"
   val logback    = "1.2.11"
   val slick      = "3.4.1"
-  val sttp       = "3.8.3"
+  val sttp       = "3.6.2"
   val tapir      = "1.1.2"
   val zioLogging = "2.1.1"
   val zioPrelude = "1.0.0-RC15"
