84513ffb819dd15843caf434b9fbc5139f1490e7
Carlos Martin <cmarting@gmail.com>
2023-08-11 15:19:57 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,11 +4,11 @@ object Versions {
   val akka       = "2.6.20"
   val h2         = "2.2.220"
   val json4s     = "4.0.6"
-  val logback    = "1.4.9"
+  val logback    = "1.4.11"
   val slick      = "3.4.1"
   val sttp       = "3.8.16"
   val zioLogging = "2.1.13"
-  val tapir      = "1.6.4"
+  val tapir      = "1.7.0"
   val zioPrelude = "1.0.0-RC19"
 
   // test
