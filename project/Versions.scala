df190b0ee7257b1cfee71fef5b22edb637bc624d
Scala Steward <scala_steward@virtuslab.com>
2023-09-18 13:56:52 +0000

    Update h2 to 2.2.224

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   // main
   val akkaHttp   = "10.2.9"
   val akka       = "2.6.20"
-  val h2         = "2.2.222"
+  val h2         = "2.2.224"
   val json4s     = "4.0.6"
   val logback    = "1.4.11"
   val slick      = "3.4.1"
