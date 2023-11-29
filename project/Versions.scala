7691982b4b2b6e835dc9624675ce6e2a5b7b6f2f
Scala Steward <scala_steward@virtuslab.com>
2023-11-29 15:26:19 +0000

    Update logback-classic to 1.4.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val akka       = "2.6.20"
   val h2         = "2.2.224"
   val json4s     = "4.0.6"
-  val logback    = "1.4.11"
+  val logback    = "1.4.13"
   val slick      = "3.4.1"
   val sttp       = "3.9.1"
   val zioLogging = "2.1.15"
