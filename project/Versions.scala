f36ba36cfc920c0faaa37a208b5fb42ef5ccefd2
Scala Steward <scala_steward@virtuslab.com>
2024-03-20 15:10:55 +0000

    Update client3:async-http-client-backend-zio, ... to 3.9.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val h2         = "2.2.224"
   val json4s     = "4.0.7"
   val logback    = "1.5.3"
-  val sttp       = "3.9.4"
+  val sttp       = "3.9.5"
   val slick      = "3.4.1"
   val zioLogging = "2.2.2"
   val tapir      = "1.10.0"
