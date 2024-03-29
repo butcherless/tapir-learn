c67baf15a9932130a6e692590a059d4db09c1d80
Carlos Martin <cmarting@gmail.com>
2024-03-29 19:54:51 +0100

    update dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,17 +1,17 @@
 object Versions {
   // main
-  val akkaHttp   = "10.2.9"
   val akka       = "2.6.20"
+  val akkaHttp   = "10.5.3"
   val h2         = "2.2.224"
   val json4s     = "4.0.7"
   val logback    = "1.5.3"
-  val sttp       = "3.9.5"
   val slick      = "3.4.1"
-  val zioLogging = "2.2.2"
+  val sttp       = "3.9.5"
   val tapir      = "1.10.0"
+  val zioLogging = "2.2.2"
   val zioPrelude = "1.0.0-RC23"
 
   // test
-  val scalatest = "3.2.18"
   val scalaMock = "5.2.0"
+  val scalatest = "3.2.18"
 }
