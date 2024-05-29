f088e78a88367ad63dc240e8f5cb843d12b0c1e1
Carlos Martin <cmarting@gmail.com>
2024-05-29 09:52:24 +0200

    refactor server endpoints

--- aviation-web/src/main/scala/com/cmartin/aviation/ZioHttpServer.scala
+++ aviation-web/src/main/scala/com/cmartin/aviation/ZioHttpServer.scala
@@ -3,19 +3,18 @@ package com.cmartin.aviation
 import com.cmartin.aviation.ApiLayer.{CountryEndpoints, SwaggerDocs}
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import zio._
-import zio.http.{HttpApp, Middleware, Server}
+import zio.http.{Middleware, Server}
 
 object ZioHttpServer
     extends ZIOAppDefault {
 
   val routeAspects = Middleware.debug ++ Middleware.timeout(5.seconds) // ++ loggingAspect
 
-  val routes =
-    ZioHttpInterpreter().toHttp(
-      //SwaggerDocs.swaggerEndpoints ++
-        CountryEndpoints.serverEndpoints
-    ) // @@ Middleware.debug // @@ routeAspects
+  val endpoints = SwaggerDocs.swaggerEndpoints ++ CountryEndpoints.serverEndpoints
 
+  val routes = ZioHttpInterpreter()
+    .toHttp(endpoints)  // @@ Middleware.debug // @@ routeAspects
+  
   // val managedErrorRoutes = ??? // mapError or catchAll
 
   // verify: http://localhost:8081/docs/
