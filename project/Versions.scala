4a1f7a4de6921b04f57d39915c196629d84f6edc
Scala Steward <scala_steward@virtuslab.com>
2024-09-14 13:39:44 +0000

    Update tapir-akka-http-server, tapir-core, ... to 1.11.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val logback    = "1.5.8"
   val slick      = "3.4.1"
   val sttp       = "3.9.8"
-  val tapir      = "1.11.2"
+  val tapir      = "1.11.3"
   val zioLogging = "2.3.1"
   val zioPrelude = "1.0.0-RC31"
 
