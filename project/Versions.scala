df84a9922ddcc205659bb9daa3256d0c8a9438f4
Carlos Martin <cmarting@gmail.com>
2024-11-26 07:22:30 +0100

    upgrade deps

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,10 +7,10 @@ object Versions {
   val json4s     = "4.0.7"
   val logback    = "1.5.12"
   val slick      = "3.4.1"
-  val tapir      = "1.11.7"
+  val tapir      = "1.11.9"
   val sttp       = "3.10.1"
-  val zioLogging = "2.3.2"
-  val zioPrelude = "1.0.0-RC31"
+  val zioLogging = "2.4.0"
+  val zioPrelude = "1.0.0-RC35"
 
   // test
   val scalaMock = "6.0.0"
