1009c91122856598be4c0022895e04dc76ee8d72
Scala Steward <scala_steward@virtuslab.com>
2026-07-12 15:46:47 +0000

    Reformat with scalafmt 3.11.3
    Executed command: scalafmt --non-interactive

--- project/BuildInfoSettings.scala
+++ project/BuildInfoSettings.scala
@@ -11,7 +11,13 @@ object BuildInfoSettings {
     SettingKey[String]("gitCommit").withRank(KeyRanks.Invisible)
 
   val value: Seq[Def.Setting[?]] = Seq(
-    buildInfoKeys    := Seq(BuildInfoKey(name), BuildInfoKey(version), BuildInfoKey(scalaVersion), BuildInfoKey(sbtVersion), BuildInfoKey(gitCommitString)),
+    buildInfoKeys    := Seq(
+      BuildInfoKey(name),
+      BuildInfoKey(version),
+      BuildInfoKey(scalaVersion),
+      BuildInfoKey(sbtVersion),
+      BuildInfoKey(gitCommitString)
+    ),
     buildInfoPackage := s"${organization.value}.api",
     buildInfoOptions ++= Seq(BuildInfoOption.ToJson, BuildInfoOption.BuildTime),
     gitCommitString  := git.gitHeadCommit.value.getOrElse("unavailable")
